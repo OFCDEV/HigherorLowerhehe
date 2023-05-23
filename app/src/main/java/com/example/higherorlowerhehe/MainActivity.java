2f53f00b527f035a4aebd2d76c76bbc09dab37f1
Debasish Bhola <devop69@gmail.com>
2023-05-23 11:03:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/higherorlowerhehe/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.higherorlowerhehe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int randomNumber;
+    public void guess(View v){
+
+        EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
+        int guessInt = Integer.parseInt(guessEditText.getText().toString());
+
+        if(guessInt > randomNumber){
+            Toast.makeText(MainActivity.this,"Lower!",Toast.LENGTH_SHORT).show();
+        } else if (guessInt < randomNumber) {
+            Toast.makeText(MainActivity.this,"Higher!",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this,"Guess is Right!",Toast.LENGTH_SHORT).show();
+        }
+
+        Toast.makeText(MainActivity.this,guessEditText.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20)+1;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
